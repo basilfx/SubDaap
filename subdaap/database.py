f0e741d2ce779935c230487a54b66c36c26ea022
Bas Stottelaar <basstottelaar@gmail.com>
2015-04-06 21:17:28 +0200

    Documentation.

--- subdaap/database.py
+++ subdaap/database.py
@@ -25,10 +25,16 @@ class Database(object):
     def get_write_cursor(self):
         """
         Get cursor instance with locking.
+
+        If the query fails due to an exception, a rollback will be performed.
+
+        :return: Cursor instance that is locked for writing.
+        :rtype: Cursor
         """
 
         with self.lock:
             cursor = self.connection.cursor(Cursor)
+
             try:
                 yield cursor
                 self.connection.commit()
@@ -42,6 +48,9 @@ class Database(object):
     def get_cursor(self):
         """
         Get cursor instance without locking.
+
+        :return: Cursor instance for reading.
+        :rtype: Cursor
         """
 
         cursor = self.connection.cursor(Cursor)
@@ -53,7 +62,9 @@ class Database(object):
 
     def create_database(self, drop_all=True):
         """
-        Create the default databases. Drop old ones if `drop_all` is True.
+        Create the default databases. Drop old ones if `drop_all` is `True`.
+
+        :param bool drop_all: Drop existing tables if they exist.
         """
 
         with self.lock:
@@ -70,7 +81,8 @@ class Database(object):
 
             # Create table query
             with self.connection:
-                self.connection.executescript(extra + """
+                self.connection.executescript(
+                    extra + """
                     CREATE TABLE IF NOT EXISTS `databases` (
                         `id` INTEGER PRIMARY KEY,
                         `persistent_id` INTEGER NOT NULL,
@@ -87,7 +99,8 @@ class Database(object):
                         `cache` tinyint(1) DEFAULT 0,
                         `checksum` int(11) NOT NULL,
                         `remote_id` int(11) DEFAULT NULL,
-                        CONSTRAINT `artist_fk_1` FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`)
+                        CONSTRAINT `artist_fk_1` FOREIGN KEY (`database_id`)
+                            REFERENCES `databases` (`id`)
                     );
                     CREATE TABLE IF NOT EXISTS `albums` (
                         `id` INTEGER PRIMARY KEY,
@@ -95,15 +108,17 @@ class Database(object):
                         `artist_id` int(11) DEFAULT NULL,
                         `name` varchar(255) NOT NULL,
                         `art` tinyint(1) DEFAULT NULL,
-                        `art_name` varchar(4096) DEFAULT NULL,
+                        `art_name` varchar(512) DEFAULT NULL,
                         `art_type` varchar(255) DEFAULT NULL,
                         `art_size` int(11) DEFAULT NULL,
                         `exclude` tinyint(1) DEFAULT 0,
                         `cache` tinyint(1) DEFAULT 0,
                         `checksum` int(11) NOT NULL,
                         `remote_id` int(11) DEFAULT NULL,
-                        CONSTRAINT `album_fk_1` FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`),
-                        CONSTRAINT `album_fk_2` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`)
+                        CONSTRAINT `album_fk_1` FOREIGN KEY (`database_id`)
+                            REFERENCES `databases` (`id`),
+                        CONSTRAINT `album_fk_2` FOREIGN KEY (`artist_id`)
+                            REFERENCES `artists` (`id`)
                     );
                     CREATE TABLE IF NOT EXISTS `items` (
                         `id` INTEGER PRIMARY KEY,
@@ -117,7 +132,7 @@ class Database(object):
                         `track` int(11) DEFAULT NULL,
                         `duration` int(11) DEFAULT NULL,
                         `bitrate` int(11) DEFAULT NULL,
-                        `file_name` varchar(4096) DEFAULT NULL,
+                        `file_name` varchar(512) DEFAULT NULL,
                         `file_type` varchar(255) DEFAULT NULL,
                         `file_suffix` varchar(32) DEFAULT NULL,
                         `file_size` int(11) DEFAULT NULL,
@@ -125,9 +140,12 @@ class Database(object):
                         `cache` tinyint(1) DEFAULT 0,
                         `checksum` int(11) NOT NULL,
                         `remote_id` int(11) DEFAULT NULL,
-                        CONSTRAINT `item_fk_1` FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`),
-                        CONSTRAINT `item_fk_2` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
-                        CONSTRAINT `item_fk_3` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`)
+                        CONSTRAINT `item_fk_1` FOREIGN KEY (`database_id`)
+                            REFERENCES `databases` (`id`),
+                        CONSTRAINT `item_fk_2` FOREIGN KEY (`album_id`)
+                            REFERENCES `albums` (`id`),
+                        CONSTRAINT `item_fk_3` FOREIGN KEY (`artist_id`)
+                            REFERENCES `artists` (`id`)
                     );
                     CREATE TABLE IF NOT EXISTS `containers` (
                         `id` INTEGER PRIMARY KEY,
@@ -141,17 +159,23 @@ class Database(object):
                         `cache` tinyint(1) DEFAULT 0,
                         `checksum` int(11) NOT NULL,
                         `remote_id` int(11) DEFAULT NULL,
-                        CONSTRAINT `container_fk_1` FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`)
-                        CONSTRAINT `container_fk_2` FOREIGN KEY (`parent_id`) REFERENCES `containers` (`id`)
+                        CONSTRAINT `container_fk_1` FOREIGN KEY (`database_id`)
+                            REFERENCES `databases` (`id`)
+                        CONSTRAINT `container_fk_2` FOREIGN KEY (`parent_id`)
+                            REFERENCES `containers` (`id`)
                     );
                     CREATE TABLE IF NOT EXISTS `container_items` (
                         `id` INTEGER PRIMARY KEY,
-                        `persistent_id` INTEGER DEFAULT 0,
                         `database_id` int(11) NOT NULL,
+                        `container_id` int(11) NOT NULL,
                         `item_id` int(11) NOT NULL,
                         `order` int(11) DEFAULT NULL,
-                        CONSTRAINT `container_item_fk_1` FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`)
-                        CONSTRAINT `container_item_fk_2` FOREIGN KEY (`container_id`) REFERENCES `containers` (`id`)
+                        CONSTRAINT `container_item_fk_1`
+                            FOREIGN KEY (`database_id`)
+                                REFERENCES `databases` (`id`)
+                        CONSTRAINT `container_item_fk_2`
+                            FOREIGN KEY (`container_id`)
+                                REFERENCES `containers` (`id`)
                     );
                     """)
 
@@ -161,10 +185,14 @@ class Cursor(sqlite3.Cursor):
     Cursor wrapper to add useful methods to the default Cursor object.
     """
 
-    def query_value(self, query, *args, **kwargs):
+    def query_value(self, query, *args):
+        """
+        """
         return self.execute(query, args).fetchone()[0]
 
     def query_dict(self, query, *args):
+        """
+        """
         result = dict()
 
         for row in self.execute(query, args):
@@ -173,4 +201,6 @@ class Cursor(sqlite3.Cursor):
         return result
 
     def query(self, query, *args):
+        """
+        """
         return self.execute(query, args)
