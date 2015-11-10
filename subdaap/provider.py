8e45350d20a89d435c85d64878334a069ca64016
Bas Stottelaar <basstottelaar@gmail.com>
2015-11-10 19:47:43 +0100

    Code improvements.

--- subdaap/provider.py
+++ subdaap/provider.py
@@ -51,6 +51,7 @@ class Provider(provider.Provider):
 
     def get_artwork_data(self, session, item):
         """
+        Get artwork data from cache or remote.
         """
 
         cache_item = self.cache_manager.artwork_cache.get(item.id)
@@ -61,40 +62,44 @@ class Provider(provider.Provider):
             self.cache_manager.artwork_cache.download(
                 item.id, cache_item, remote_fd)
 
+            logger.debug("Artwork data from remote, size=unknown")
             return cache_item.iterator(), None, None
+
+        logger.debug(
+            "Artwork data from cache, size=%d", cache_item.size)
         return cache_item.iterator(), None, cache_item.size
 
     def get_item_data(self, session, item, byte_range=None):
         """
+        Get item data from cache or remote.
         """
 
         cache_item = self.cache_manager.item_cache.get(item.id)
-
-        the_connection = self.connections[item.database_id]
-        is_transcode = the_connection.needs_transcoding(item.file_suffix)
-        
+        connection = self.connections[item.database_id]
+        is_transcode = connection.needs_transcoding(item.file_suffix)
         item_file_type = item.file_type
-        
+
         if is_transcode:
-            item_file_type = the_connection.transcode_format[item.file_type]
+            item_file_type = connection.transcode_format[item.file_type]
 
         if cache_item.iterator is None:
-            remote_fd = the_connection.get_item_fd(
+            remote_fd = connection.get_item_fd(
                 item.remote_id, item.file_suffix)
             self.cache_manager.item_cache.download(
                 item.id, cache_item, remote_fd)
-
             item_size = item.file_size
-            # Determine returned size by checking for transcode
+
+            # Determine returned size by checking for transcode.
             if is_transcode:
                 item_size = -1
 
-            logger.info("[get_item_data:new] range: {} type: {} size: {}".
-                        format(byte_range, item_file_type, item_size))
+            logger.debug(
+                "Item data from remote: range=%s, type=%s, size=%d",
+                byte_range, item_file_type, item_size)
+            return cache_item.iterator(byte_range), item_file_type, item_size
 
-            return cache_item.iterator(byte_range), item_file_type, \
-                    item_size
-        logger.info("[get_item_data:cached] range: {} type: {} size: {}".
-                    format(byte_range, item_file_type, item.file_size))
+        logger.debug(
+            "Item data from cache, range=%s, type=%s, size=%d",
+            byte_range, item.file_type, item.file_size)
         return cache_item.iterator(byte_range), item_file_type, \
             cache_item.size
